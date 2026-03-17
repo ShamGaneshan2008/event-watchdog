6118457aa03addd1db76b97db149b316007aa489
J I N || S A K A I <shamganeshansb@gmail.com>
2026-03-17 14:55:43 +0530

    Delete Exercies directory

--- Exercies/example.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-connection = sqlite3.connect("../Sham.db")
-cursor = connection.cursor()
-
-cursor.execute("SELECT * FROM events WHERE date='2088-10-15'")
-rows = cursor.fetchall()
-print(rows)
